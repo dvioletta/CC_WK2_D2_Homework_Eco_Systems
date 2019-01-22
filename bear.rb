d06e8b7c0e6a2e863e41de4ef283af3551f0f6ae
dvioletta <Anita@Pixie.local>
2019-01-22 21:33:35 +0000

    Update of Bear section

--- bear.rb
+++ bear.rb
@@ -1,5 +1,7 @@
 class Bear
 
+attr_reader :name, :type, :stock_pile
+
   def initialize (name, type)
     @name = name
     @type = type
@@ -14,6 +16,19 @@ class Bear
     return @type
   end
 
+  def add_fish(fish)
+    @stock_pile << fish
+  end
 
+  def remove_fish(fish)
+    @stock_pile.delete(fish)
+  end
+
+  def add_fish_to_bear__remove_from_river(fish)
+    for fish in river()
+      add_fish(stock_pile)
+    end
+    stop.clear_river()
+  end
 
 end
--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -19,9 +19,28 @@ class BearTest < MiniTest::Test
   assert_equal("Yogi", @bear_1.get_name)
   end
 
-  def test_get_name
-  assert_equal("Grizzly", @bear_1.get_name)
+  def test_get_type
+  assert_equal("Grizzly", @bear_1.get_type)
   end
 
+  def test_add_fish
+    @bear_1.add_fish(@fish_1)
+    assert_equal(1, @bear_1.stock_pile.count)
+  end
+
+  def test_remove_fish
+    @bear_1.remove_fish(@fish_1)
+    assert_equal(0, @bear_1.stock_pile.count)
+  end
+
+
+  def test_add_fish_to_bear__remove_from_river()
+  @bear_1.add_fish(@fish_3)
+  @river_1 = River.new("Amazon")
+  @river_1.add_fish(@fish_2)
+  @river_1.add_fish(@fish_2)
+  @bear_1.remove_fish(@river_1)
+  assert_equal(3, @bear_1.shock_pile.count())
+  end
 
 end
